0d2d719fc355f0d431a95c13b2c33bc410598dba
Scarlet Erazo <2509073@upy.edu.mx>
2026-06-25 20:13:49 -0600

    classwork 09

---
+++ CW9/Spanish verb conjugator .py
@@ -0,0 +1,34 @@
+
+# Classwork 09 - Spanish Verb Conjugator
+# Author: Scarlet Verenice Erazo Aranda
+# Universidad Politecnica de Yucatan - Q2 2026
+
+
+# # INPUT
+verb = input("Enter verb: ")
+
+# # PROCESS
+
+# List of pronouns (required structure)
+pronouns = ['yo', 'tu', 'el', 'nosotros', 'vosotros', 'ellos']
+
+# Dictionary of endings by verb type (required structure)
+endings = {
+    'ar': ['o', 'as', 'a', 'amos', 'ais', 'an'],
+    'er': ['o', 'es', 'e', 'emos', 'eis', 'en'],
+    'ir': ['o', 'es', 'e', 'imos', 'is', 'en']
+}
+
+# Get stem (verb minus last 2 letters) and ending (last 2 letters)
+stem   = verb[:-2]
+ending = verb[-2:]
+
+# Look up the matching endings list in the dictionary
+endings_list = endings[ending]
+
+# # OUTPUT
+
+# Loop over pronouns and print conjugation using enumerate
+for index, pronombre in enumerate(pronouns):
+    terminacion = endings_list[index]
+    print(f"{pronombre} {stem}{terminacion}")
